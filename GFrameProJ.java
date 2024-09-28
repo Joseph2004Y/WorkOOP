3821e6d825bc43e26a178b1ae9a5779408b3ec46
๋Joe 2004Y <josavejoe@gmail.com>
2024-09-28 18:21:31 +0700

    ได้รับการแก้ไข
    Signed-off-by: ๋Joe 2004Y <josavejoe@gmail.com>

--- GFrameProJ.java
+++ GFrameProJ.java
@@ -9,131 +9,122 @@ import javax.swing.*;
 import javax.swing.Timer;
 
 public class GFrameProJ extends JFrame {
-    
-    //-------------------------------Set up Main Frame Start--------------------------------------------------
+
+    //Set up Main Frame Start
     public GFrameProJ() {
-        setSize(1200, 700);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setLayout(null);
+        setSize(1200, 700);  
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        setLocationRelativeTo(null); 
+        setLayout(null);  
 
-        Panel4Paint panel4Paint = new Panel4Paint();
-        add(panel4Paint);
-        setVisible(true);
+        Panel4Paint panel4Paint = new Panel4Paint(); 
+        add(panel4Paint);  
+        setVisible(true);  
     }
-    //------------------------------Set up Main Frame End-------------------------------------------------
-
-
-    //--------------------------------Main method 4 Run Start-------------------------------------------------------- 
     public static void main(String[] args) {
-        GFrameProJ gf = new GFrameProJ();
+        GFrameProJ gf = new GFrameProJ();  
     }
 }
-    //--------------------------------Main method 4 Run End-------------------------------------------------------- 
+//===========================================================================================================================================
 
-
-
-
-
-
-
-    //----------------------------------Panel 4 Pain :D Start-----------------------------------------------------
 class Panel4Paint extends JPanel {
-    //////////////Attribute/////////////////////
-    private ArrayList<Meteor> meteorsArL;
-    Random ran = new Random();
-    int numOfImg = 0;
 
+        private ArrayList<Meteor> meteorsArL;  // เก็บข้อมูลอุกกาบาตทั้งหมดในรูปแบบ ArrayList
+        private Random ran = new Random();  // สร้างออบเจ็กต์สุ่มค่าต่างๆ
+        private int numOfImg = 0;  // จำนวนอุกกาบาต
+        private Image BGI;  // รูปภาพพื้นหลัง
     
-    Image BGI = Toolkit.getDefaultToolkit().createImage(
-        System.getProperty("user.dir") + File.separator + "imgProject" + File.separator + "world.jpg");
+        // คอนสตรัคเตอร์สำหรับแผงวาดภาพ
+        public Panel4Paint() {
 
-    //////////////Attribute/////////////////////
-
-
-
-    public Panel4Paint() {
-
-        setSize(1200, 700);//set up Panel
+        setSize(1200, 700);  // กำหนดขนาดแผง
         setLayout(null);
+    
+            BGI = Toolkit.getDefaultToolkit().createImage(
+                System.getProperty("user.dir") + File.separator + "imgProject" + File.separator + "world.jpg");
+    
+            meteorsArL = new ArrayList<>();  // สร้าง ArrayList สำหรับเก็บอุกกาบาต
+            Scanner input = new Scanner(System.in);  // รับข้อมูลจากผู้ใช้ผ่านคอนโซล
+    
+            try {
 
-        meteorsArL = new ArrayList<>();//Local Variable
-        Scanner input = new Scanner(System.in);
-
-
-
-        try {
-            System.out.print("Enter number of Meteors: ");
-            numOfImg = input.nextInt();
-
-            for (int i = 0; i < numOfImg; i++) {
-                meteorsArL.add(new Meteor(ran.nextInt(1000), ran.nextInt(500), //create Meteor
-                    ran.nextInt(5) - 2, ran.nextInt(5) - 2, ran.nextInt(10) + 1));
-            }
-
-
-            //Timer 4 check movement Meteor
-            Timer timer = new Timer(100, new ActionListener(){
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    for (Meteor MTO : meteorsArL) {
-                        MTO.meteorMove(getWidth(), getHeight(), meteorsArL);
-                    }
-                    repaint();
+                System.out.print("Enter number of Meteors: ");
+                numOfImg = input.nextInt();
+    
+                for (int i = 0; i < numOfImg; i++) {
+        
+                    Meteor meteor = new Meteor(ran.nextInt(1000), ran.nextInt(500),
+                        ran.nextInt(5) - 2, ran.nextInt(5) - 2, ran.nextInt(10) + 1, this);
+                    meteorsArL.add(meteor);  // เพิ่มอุกกาบาตเข้าใน ArrayList
+    
+                    // สร้าง thread สำหรับอุกกาบาตแต่ละตัว
+                    Thread meteorThread = new Thread(meteor);
+                    meteorThread.start(); 
                 }
-            });
-            timer.start();
-
-        } catch (InputMismatchException ex) {
-            System.out.println("Error: " + ex.getMessage());
-        }
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        
-        g.drawImage(BGI, 0, 0, getWidth(), getHeight(), this);
-
-        
-        for (Meteor MTO : meteorsArL) {
-            if(MTO.getisDestroy()){
-                g.drawImage(MTO.getDestroyImg(), MTO.getXMeto(), MTO.getYMeto(), 100, 100, this);//Change meteor to bomb
-            } else {
-                g.drawImage(MTO.getMTOImage(), MTO.getXMeto(), MTO.getYMeto(), 100, 100, this);//drawn Meteor
+    
+            } catch (InputMismatchException ex) {
+                System.out.println("Error: " + ex.getMessage()); 
             }
         }
+    
 
-        //Check mouse clicked 2 time
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if (e.getClickCount() == 2) {
-                    for (Meteor MTO : meteorsArL) {
-                        if (MTO.isCollidingWithMouse(e.getX(), e.getY())) {
-                            MTO.setisDestroy(); 
-                        
-                            repaint();
-                                
-                
-                            //set alarm bomb 2 second after double clicked Meteor
-                            Timer alarm = new Timer(1000, new ActionListener() {
-                                @Override
-                                public void actionPerformed(ActionEvent e) {
-                                    meteorsArL.remove(MTO);//delete Meteor from ArrayList
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
 
-                                    repaint();
-                                        
-                                }
-                            });
-                            alarm.setRepeats(false);//set Timer work 1 time **repeat = ทำซ้ำ**
-                            alarm.start();
-                            break;
+            g.drawImage(BGI, 0, 0, getWidth(), getHeight(), this); 
+    
+            // วาดอุกกาบาตทั้งหมดใน
+            for (Meteor MTO : meteorsArL) {
+                if (MTO.getisDestroy()) {
+
+                    // ให้BOOM
+                    g.drawImage(MTO.getDestroyImg(), MTO.getXMeto(), MTO.getYMeto(), 100, 100, this);
+
+                } 
+                else {
+
+                    // หากอุกกาบาตยังไม่ถูกทำลาย ให้แสดงภาพอุกกาบาต
+                    g.drawImage(MTO.getMTOImage(), MTO.getXMeto(), MTO.getYMeto(), 100, 100, this);
+
+                }
+            }
+    
+
+           
+            addMouseListener(new MouseAdapter() { // ตรวจจับการคลิกเมาส์
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    if (e.getClickCount() == 2) {  // ตรวจสอบว่าคลิกสองครั้ง
+                        for (Meteor MTO : meteorsArL) {
+                            if (MTO.isCollidingWithMouse(e.getX(), e.getY())) {  // ตรวจสอบว่าเมาส์ชนกับอุกกาบาตหรือไม่
+                                MTO.setisDestroy();  
+                                repaint();  
+    
+                                // ลบอุกกาบาตออกจากหน้าจอ
+                                Timer alarm = new Timer(500, new ActionListener() {
+
+                                    @Override
+                                    public void actionPerformed(ActionEvent e) {
+                                        MTO.stopRunning();  
+                                        meteorsArL.remove(MTO); 
+                                        repaint(); 
+                                    }
+
+                                });
+                                alarm.setRepeats(false); 
+                                alarm.start(); 
+                                break;
+                            }
                         }
                     }
                 }
-            }
-        });
+            });
+        }
+    
+        // คืนค่า ArrayList ของอุกกาบาต
+        public ArrayList<Meteor> getMeteors() {
+            return meteorsArL;
+        }
     }
-}
-    //----------------------------------Panel 4 Pain :D End-----------------------------------------------------
+
--- Meteor.java
+++ Meteor.java
@@ -4,88 +4,94 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.ImageIcon;
 
-//-----------------------------Class of Attribute Meteor------------------------
-// 1 class can make many Meteor
-public class Meteor {
-    ////////////Attribute///////////
+public class Meteor extends Thread {
+
     public int xStart, yStart;
     public int MTOspeedX, MTOspeedY;
     public int numOfImgMTO;
     private boolean isDestroy = false;
     private int sizeOfMeteor = 100;
+    private boolean running = true; // To control thread
     Random random = new Random();
-    
+
     Image imgMTO;
     Image DestroyMeteor;
-    ////////////Attribute///////////
+    private Panel4Paint panel; // Reference to panel for repainting
 
-
-
-
-    ///////////////             Constructor             ////////////////////////////////////////////
-    public Meteor(int x, int y, int spdX, int spdY, int numImgMTO) {
+    //Constructor 
+    public Meteor(int x, int y, int spdX, int spdY, int numImgMTO, Panel4Paint panel) {
         this.xStart = x;
         this.yStart = y;
         this.MTOspeedX = spdX;
         this.MTOspeedY = spdY;
         this.numOfImgMTO = numImgMTO;
-        
+        this.panel = panel; // Save panel reference to call repaint
+
+
         imgMTO = new ImageIcon(System.getProperty("user.dir") + File.separator + 
             "imgProject" + File.separator + numOfImgMTO + ".png").getImage();
 
         DestroyMeteor = new ImageIcon(System.getProperty("user.dir") + File.separator + 
             "imgProject" + File.separator + "bomb.gif").getImage();
-
     }
-    ///////////////             Constructor             ////////////////////////////////////////////
 
+    // Thread run 
+    @Override
+    public void run() {
 
-    //////////////              Method          -///////////////////////////////////////
+        while (running) {
+
+            meteorMove(panel.getWidth(), panel.getHeight(), panel.getMeteors());
+            panel.repaint();
+
+            try {
+                int sleep = random.nextInt(50); //ช้าเร็วของอุกาบาต
+                Thread.sleep(sleep); 
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stopRunning() {
+        running = false;
+    }
+
+    // Meteor Movement Methods
     public void meteorMove(int width, int height, ArrayList<Meteor> meteors) {
-        
-        //bounce check in X
         if (xStart <= 0 || xStart >= width - getSizeOfMeteor()) {
             MTOspeedX = -MTOspeedX;
             adjustSpeed();
-            //set meteor in frame
             xStart = Math.max(0, Math.min(xStart, width - getSizeOfMeteor()));
         }
-        //bounce check in Y
+
         if (yStart <= 0 || yStart >= height - getSizeOfMeteor()) {
             MTOspeedY = -MTOspeedY;
             adjustSpeed();
-            //set meteor in frame
             yStart = Math.max(0, Math.min(yStart, height - getSizeOfMeteor()));
         }
 
-        // bounce between Meteor
         for (Meteor MTO : meteors) {
             if (MTO != this && isColliding(MTO)) {
                 resolveCollision(MTO);
             }
         }
 
-        // update position Meteor
         xStart += MTOspeedX;
         yStart += MTOspeedY;
-
-        // check Meteor isInframe again
         xStart = Math.max(0, Math.min(xStart, width - getSizeOfMeteor()));
         yStart = Math.max(0, Math.min(yStart, height - getSizeOfMeteor()));
     }
 
-    // check bounce by size of meteor
     public boolean isColliding(Meteor MTO) {
         int distance = (int) Math.sqrt(Math.pow(xStart - MTO.xStart, 2) + Math.pow(yStart - MTO.yStart, 2));
-        return distance < getSizeOfMeteor(); // distance = ระยะชน
+        return distance < getSizeOfMeteor();
     }
 
-    // check overlap Meteor --ซ้อนกัน--
     public void resolveCollision(Meteor MTO) {
         int overlap = getSizeOfMeteor() - (int) Math.sqrt(Math.pow(xStart - MTO.xStart, 2) + Math.pow(yStart - MTO.yStart, 2));
 
-        
-        //change direction after bounce
         int dx = xStart - MTO.xStart;
         int dy = yStart - MTO.yStart;
 
@@ -99,16 +105,13 @@ public class Meteor {
             MTO.yStart -= (dy / Math.abs(dy)) * overlap / 2;
         }
 
-        // after bounce change direction
         MTOspeedX = -MTOspeedX;
         MTO.MTOspeedX = -MTO.MTOspeedX;
         MTOspeedY = -MTOspeedY;
         MTO.MTOspeedY = -MTO.MTOspeedY;
     }
 
-    // adjust Speed After bounce between Meteor
     public void adjustSpeed() {
-        // speed
         do {
             MTOspeedX = random.nextInt(30) - 5;
         } while (MTOspeedX == 0);
@@ -118,15 +121,11 @@ public class Meteor {
         } while (MTOspeedY == 0);
     }
 
-
-    // check point clicked in area meteor
     public boolean isCollidingWithMouse(int mouseX, int mouseY) {
         return (mouseX >= xStart && mouseX <= xStart + 100 && 
                 mouseY >= yStart && mouseY <= yStart + 100);
     }
 
-    
-    /////////////////     Geter Seter Medthod     /////////////////////
     public int getXMeto() {
         return xStart;
     }
@@ -139,28 +138,19 @@ public class Meteor {
         return imgMTO;
     }
 
-    public Image getDestroyImg(){
+    public Image getDestroyImg() {
         return DestroyMeteor;
     }
 
-    public boolean getisDestroy(){
+    public boolean getisDestroy() {
         return isDestroy;
     }
 
-    public int getSizeOfMeteor(){
+    public int getSizeOfMeteor() {
         return sizeOfMeteor;
     }
 
-    public void setisDestroy(){
+    public void setisDestroy() {
         isDestroy = true;
     }
-
-    public void resetisDestroy(){
-        isDestroy = false;
-    }
-
-    /////////////////     Geter Seter Medthod     /////////////////////
-
-    //////////////              Method          -///////////////////////////////////////
-
 }
